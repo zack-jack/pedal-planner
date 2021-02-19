d2b65abeee60bb545b0f8b7cc6bf9fa7765e4cff
zack-jack <zjackson@redventures.com>
2021-02-19 10:19:03 -0500

    refactor components layout

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
 
-import AppHeader from './components/AppHeader';
-import Drawer from './components/Drawer';
 import Home from './views/Home';
 import Contact from './views/Contact';
 
-import mockPedals from './data/pedals.json';
-import mockPedalboards from './data/pedalboards.json';
-
 export default function App() {
-  const [showPedals, setShowPedals] = useState(false);
-  const [showPedalboards, setShowPedalboards] = useState(false);
-  const [pedals, setPedals] = useState([]);
-  const [pedalboards, setPedalboards] = useState([]);
-
-  const getPedals = useCallback(() => {
-    setPedals(mockPedals);
-  }, []);
-
-  const getPedalboards = useCallback(() => {
-    setPedalboards(mockPedalboards);
-  }, []);
-
-  useEffect(() => {
-    getPedals();
-    getPedalboards();
-  }, [getPedals, getPedalboards, pedals, pedalboards]);
-
-  const toggleDrawer = (btnId) => {
-    if (btnId.indexOf('add-pedal') > -1) {
-      setShowPedals(!showPedals);
-      setShowPedalboards(false);
-    }
-
-    if (btnId.indexOf('pedalboard') > -1) {
-      setShowPedalboards(!showPedalboards);
-      setShowPedals(false);
-    }
-  };
-
-  const closeDrawer = () => {
-    setShowPedals(false);
-    setShowPedalboards(false);
-  };
-
   return (
     <Router>
       <Switch>
@@ -56,18 +16,6 @@ export default function App() {
           <Contact />
         </Route>
         <Route path="/">
-          <AppHeader
-            showPedals={showPedals}
-            showPedalboards={showPedalboards}
-            toggleDrawer={toggleDrawer}
-          />
-          <Drawer
-            closeDrawer={closeDrawer}
-            pedals={pedals}
-            pedalboards={pedalboards}
-            showPedals={showPedals}
-            showPedalboards={showPedalboards}
-          />
           <Home />
         </Route>
       </Switch>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import AppHeader from '../components/AppHeader';
 import Canvas from '../components/Canvas';
+import Drawer from '../components/Drawer';
 
-const Home = () => (
-  <main>
-    <Canvas />
-  </main>
-);
+import mockPedals from '../data/pedals.json';
+import mockPedalboards from '../data/pedalboards.json';
+
+const Home = () => {
+  const [showPedals, setShowPedals] = useState(false);
+  const [showPedalboards, setShowPedalboards] = useState(false);
+  const [pedals, setPedals] = useState([]);
+  const [pedalboards, setPedalboards] = useState([]);
+
+  const getPedals = useCallback(() => {
+    setPedals(mockPedals);
+  }, []);
+
+  const getPedalboards = useCallback(() => {
+    setPedalboards(mockPedalboards);
+  }, []);
+
+  useEffect(() => {
+    getPedals();
+    getPedalboards();
+  }, [getPedals, getPedalboards, pedals, pedalboards]);
+
+  const toggleDrawer = (btnId) => {
+    if (btnId.indexOf('add-pedal') > -1) {
+      setShowPedals(!showPedals);
+      setShowPedalboards(false);
+    }
+
+    if (btnId.indexOf('pedalboard') > -1) {
+      setShowPedalboards(!showPedalboards);
+      setShowPedals(false);
+    }
+  };
+
+  const closeDrawer = () => {
+    setShowPedals(false);
+    setShowPedalboards(false);
+  };
+
+  return (
+    <main>
+      <AppHeader
+        showPedals={showPedals}
+        showPedalboards={showPedalboards}
+        toggleDrawer={toggleDrawer}
+      />
+      <Drawer
+        closeDrawer={closeDrawer}
+        pedals={pedals}
+        pedalboards={pedalboards}
+        showPedals={showPedals}
+        showPedalboards={showPedalboards}
+      />
+      <Canvas />
+    </main>
+  );
+};
 
 export default Home;
